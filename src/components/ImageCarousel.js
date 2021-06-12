ecc6b2d4f2cf3400d3911b33b6441a2b926aa5c8
eric santos <eric355402@yahoo.com>
2021-06-12 16:35:52 -0600

    added image and styled carousel

--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -4,15 +4,21 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "../styles/imageCarousel.css";
 
 const ImageCarousel = () => (
-  <Carousel showArrows={true} autoPlay className="carousel">
+  <Carousel
+    showArrows={true}
+    infiniteLoop={true}
+    showThumbs={false}
+    autoPlay
+    className="carousel"
+  >
     <div>
-      <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-1.jpg" />
+      <img alt="" src="images/placeholderFish.jpg" />
     </div>
     <div>
-      <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-1.jpg" />
+      <img alt="" src="images/placeholderFish.jpg" />
     </div>
     <div>
-      <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-1.jpg" />
+      <img alt="" src="images/placeholderFish.jpg" />
     </div>
   </Carousel>
 );
