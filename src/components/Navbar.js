b80441f232b8da7cfd68f39455d67f56e49592bc
eric santos <eric355402@yahoo.com>
2021-06-12 17:02:00 -0600

    added mobile menu section, needs functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { HiOutlineMenu } from "react-icons/hi";
 import "../styles/navbar.css";
 
 
 
 const Navbar = () => {
   return (
-    <nav>
-      <Link to="/" className="link">
-        <h1>Home</h1>
-      </Link>
-      <Link to="/menu" className="link">
-        <h1>Menu</h1>
-      </Link>
-      <Link to="/specials" className="link">
-        <h1>Specials</h1>
-      </Link>
-      <Link to="/about" className="link">
-        <h1>About</h1>
-      </Link>
-      <Link to="/contact" className="link">
-        <h1>Contact</h1>
-      </Link>
-      <Link to="/careers" className="link">
-        <h1>Careers</h1>
-      </Link>
-    </nav>
+    <>
+      <nav>
+        <Link to="/" className="link">
+          <h1>Home</h1>
+        </Link>
+        <Link to="/menu" className="link">
+          <h1>Menu</h1>
+        </Link>
+        <Link to="/specials" className="link">
+          <h1>Specials</h1>
+        </Link>
+        <Link to="/about" className="link">
+          <h1>About</h1>
+        </Link>
+        <Link to="/contact" className="link">
+          <h1>Contact</h1>
+        </Link>
+        <Link to="/careers" className="link">
+          <h1>Careers</h1>
+        </Link>
+      </nav>
+      <div className="mobile-menu-bar">
+        <h1>Logo</h1>
+        <HiOutlineMenu className="menu-icon" size='30' />
+      </div>
+    </>
   );
 };
 export default Navbar;
