f7c62a808d006f716a6c057c108a5674a3ce4fce
eric santos <eric355402@yahoo.com>
2021-06-12 17:12:37 -0600

    added more featured items with lorem ipsum

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../styles/home.css";
 import Navbar from "./Navbar";
+import {Link} from 'react-router-dom'
 import ImageCarousel from "./ImageCarousel";
 
 const Home = () => {
@@ -12,8 +13,41 @@ const Home = () => {
           <ImageCarousel />
         </div>
         <div className="featured">
-          <h1>Featured Section</h1>
+          <h1>Featured Item 1</h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam,
+            culpa nihil harum magnam dolor perferendis aliquam sit vitae dolores
+            ipsam laboriosam tenetur. Ea consectetur nobis incidunt minus esse
+            praesentium, distinctio aliquam nulla corrupti repellat, voluptatem
+            neque voluptate molestias nemo porro!
+          </p>
+          <h1>Featured Item 2</h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam,
+            culpa nihil harum magnam dolor perferendis aliquam sit vitae dolores
+            ipsam laboriosam tenetur. Ea consectetur nobis incidunt minus esse
+            praesentium, distinctio aliquam nulla corrupti repellat, voluptatem
+            neque voluptate molestias nemo porro!
+          </p>
+          <h1>Featured Item 3</h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam,
+            culpa nihil harum magnam dolor perferendis aliquam sit vitae dolores
+            ipsam laboriosam tenetur. Ea consectetur nobis incidunt minus esse
+            praesentium, distinctio aliquam nulla corrupti repellat, voluptatem
+            neque voluptate molestias nemo porro!
+          </p>
+          <h1>Featured Item 4</h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam,
+            culpa nihil harum magnam dolor perferendis aliquam sit vitae dolores
+            ipsam laboriosam tenetur. Ea consectetur nobis incidunt minus esse
+            praesentium, distinctio aliquam nulla corrupti repellat, voluptatem
+            neque voluptate molestias nemo porro!
+          </p>
+          <Link to='menu'><button>See Our Menu</button></Link>
         </div>
+        
       </main>
     </>
   );
