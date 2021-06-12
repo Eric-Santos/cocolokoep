0ae8b00b16d45868b772909d96d7190b241ba019
eric santos <eric355402@yahoo.com>
2021-06-11 18:19:21 -0600

    added mobile ready images on srcset

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -8,64 +8,84 @@ const Menu = () => {
       <Navbar />
       <picture>
         <img
+          srcset="images/menu0small.wepb 324w,
+             images/menu0.webp 400w"
+          sizes="(max-width: 600px) 324px,
+            400px"
           src="images/menu0.webp"
-          alt="menu page 1"
-          height="1529"
-          width="1204"
+          alt="first page of menu"
         />
         <img
+          srcset="images/menu1small.wepb 324w,
+             images/menu1.webp 400w"
+          sizes="(max-width: 600px) 324px,
+            400px"
           src="images/menu1.webp"
-          alt="menu page 1"
-          height="1529"
-          width="1204"
+          alt="first page of menu"
         />
         <img
+          srcset="images/menu2small.wepb 324w,
+             images/menu2.webp 400w"
+          sizes="(max-width: 600px) 324px,
+            400px"
           src="images/menu2.webp"
-          alt="menu page 1"
-          height="1529"
-          width="1204"
+          alt="first page of menu"
         />
         <img
+          srcset="images/menu3small.wepb 324w,
+             images/menu3.webp 400w"
+          sizes="(max-width: 600px) 324px,
+            400px"
           src="images/menu3.webp"
-          alt="menu page 1"
-          height="1529"
-          width="1204"
+          alt="first page of menu"
         />
         <img
+          srcset="images/menu4small.wepb 324w,
+             images/menu4.webp 400w"
+          sizes="(max-width: 600px) 324px,
+            400px"
           src="images/menu4.webp"
-          alt="menu page 1"
-          height="1529"
-          width="1204"
+          alt="first page of menu"
         />
         <img
+          srcset="images/menu5small.wepb 324w,
+             images/menu5.webp 400w"
+          sizes="(max-width: 600px) 324px,
+            400px"
           src="images/menu5.webp"
-          alt="menu page 1"
-          height="1529"
-          width="1204"
+          alt="first page of menu"
         />
         <img
+          srcset="images/menu6small.wepb 324w,
+             images/menu6.webp 400w"
+          sizes="(max-width: 600px) 324px,
+            400px"
           src="images/menu6.webp"
-          alt="menu page 1"
-          height="1529"
-          width="1204"
+          alt="first page of menu"
         />
         <img
+          srcset="images/menu7small.wepb 324w,
+             images/menu7.webp 400w"
+          sizes="(max-width: 600px) 324px,
+            400px"
           src="images/menu7.webp"
-          alt="menu page 1"
-          height="1529"
-          width="1204"
+          alt="first page of menu"
         />
         <img
+          srcset="images/menu8small.wepb 324w,
+             images/menu8.webp 400w"
+          sizes="(max-width: 600px) 324px,
+            400px"
           src="images/menu8.webp"
-          alt="menu page 1"
-          height="1529"
-          width="1204"
+          alt="first page of menu"
         />
         <img
+          srcset="images/menu9small.wepb 324w,
+             images/menu9.webp 400w"
+          sizes="(max-width: 600px) 324px,
+            400px"
           src="images/menu9.webp"
-          alt="menu page 1"
-          height="1529"
-          width="1204"
+          alt="first page of menu"
         />
       </picture>
     </div>
