0e14d24a6c93c5cf1c15741f9bdc1ca83a1a9c90
eric santos <eric355402@yahoo.com>
2021-06-24 14:31:01 -0600

    created about page with 800w image

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,26 @@
 import React from "react";
+import "../styles/about.css";
 import Navbar from "./Navbar";
 
 const About = () => {
   return (
-    <div>
+    <>
       <Navbar />
-      <h1>About</h1>
-    </div>
+      <div className="about">
+        <img src="images/coco-800w.webp" alt="banner" />
+        <p>
+          Como todo Negocio, Coco Loko empieza como solo una idea nueva de
+          jovenes que desean ofrecer un lugar tranquilo, con buena comida, buen
+          ambiente y Refrescantes Clamatos y Cervezas, Un lugar donde puedas
+          venir a "Curarte" la cruda y disfrutar los mejores mariscos, Clamatos
+          y botanas de la Ciudad. De antemano les agradecemos su preferencia y
+          desde luego sus comentarios y opiniones siempre son bienvenidos,
+          Esperamos de todo Corazon que sea de su agrado y que nos apoyen en
+          este proyecto para seguir ofreciendo nuestros servicios a Nuestra
+          Hermosa Ciudad y a Su Gente!
+        </p>
+      </div>
+    </>
   );
 };
 export default About;
