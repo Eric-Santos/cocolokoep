7a26c3b70637999876951314c48fcd1e10748a9a
eric santos <eric355402@yahoo.com>
2021-06-24 03:27:50 -0600

    added images to manifest.json for pwa

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/footer.css";
-import { FaFacebookSquare, FaSnapchat, FaInstagram } from "react-icons/fa";
+import { FaFacebookSquare, FaInstagram } from "react-icons/fa";
 import { SiTiktok } from "react-icons/si";
 
 const Footer = () => {
