15a853914d2fa8eeba5107767ed66189431a39ee
eric santos <eric355402@yahoo.com>
2021-06-24 03:09:23 -0600

    added social media icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,29 +6,53 @@ import { SiTiktok } from "react-icons/si";
 const Footer = () => {
   return (
     <>
-    <footer>
-      <h1>Logo</h1>
-      <h1>Follow Us</h1>
-      <div className="icons">
-        <SiTiktok size="30" className="icon" />
-        <FaFacebookSquare size="30" className="icon" />
-        <FaInstagram size="30" className="icon" />
-        <FaSnapchat size="30" className="icon" />
+      <footer>
+        <h1>Logo</h1>
+        <h1>Follow Us</h1>
+        <div className="icons">
+          <a
+            href="https://www.tiktok.com/@coco.lokoep?lang=en"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <SiTiktok size="30" className="icon" />
+          </a>
+
+          <a
+            href="https://www.facebook.com/CocolokoElPaso/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <FaFacebookSquare size="30" className="icon" />
+          </a>
+          <a
+            href="https://www.instagram.com/coco.lokoep/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <FaInstagram size="30" className="icon" />
+          </a>
+          {/* <a
+            href="https://www.snapchat.com/coco.loko17/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <FaSnapchat size="30" className="icon" />
+          </a> */}
+        </div>
+      </footer>
+      <div className="copyright">
+        <h4>
+          &copy; Copyright 2021 | Crafted By
+          <a
+            href="https://www.linkedin.com/in/ericsantos01/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Eric Santos
+          </a>
+        </h4>
       </div>
-      
-    </footer>
-    <div className="copyright">
-    <h4>
-      &copy; Copyright 2021 | Crafted By{" "}
-      <a
-        href="https://www.linkedin.com/in/ericsantos01/"
-        target="_blank"
-        rel="noreferrer noopener"
-      >
-        Eric Santos
-      </a>
-    </h4>
-  </div>
     </>
   );
 };
